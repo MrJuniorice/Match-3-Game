5c29b15ee2088a98f2169e05fa8be609af35a68e
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-04-29 04:59:48 -0400

    First

--- Match 3/Library/PackageCache/com.unity.platforms.android@0.3.0-preview.4/Runtime/Unity.Tiny.InputAndroid/bee~/InputAndroidCustomizer.cs
+++
@@ -1,9 +0,0 @@
-using JetBrains.Annotations;
-
-[UsedImplicitly]
-class CustomizerForInputAndroid : AsmDefCSharpProgramCustomizer
-{
-    public override string CustomizerFor => "Unity.Tiny.InputAndroid";
-
-    public override string[] ImplementationFor => new [] { "Unity.Tiny.Input" };
-}
